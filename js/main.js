1767aefa7ffaf81c0316f2dadd28b48ff80d35de
Vinicius <vfarias.designer@gmail.com>
2023-12-28 12:22:56 -0300

    adjust index

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 import { menuBarMobile } from "./mobileNavBar.js";
 import { scroll } from "./scroll.js"
 import { animeSectionScroll } from "./animationSections.js";
-import { createArticleServices } from "./createSlideHtml.js"
+import { createArticleServices } from "./createSlideHtml.js";
 
 
 menuBarMobile();
 scroll();
 animeSectionScroll();
-createArticleServices()
+createArticleServices();
